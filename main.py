b3ac29de3f3cb764419cefcc0f03280e5cb0874a
Sukhdev Shiyani <sukhdevpersonal2004@gmail.com>
2026-09-11 16:14:31 +0530

    Add new allowed origin for CORS

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://web-to-apk-smoky.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
